2832e6ee9bc49fc865d1c53b0f0d0413c7795757
Sourodeep Chatterjee <sourodeep224@outlook.com>
2020-07-19 12:55:27 +0530

    Removed @types/webpack-merge for merge v5, fixed type issues

--- configs/webpack/webpack.dev.ts
+++ configs/webpack/webpack.dev.ts
@@ -1,7 +1,16 @@
 import webpack from "webpack";
-import webpackMerge from "webpack-merge";
+import { merge as webpackMerge } from "webpack-merge";
+import WebpackDevServer from "webpack-dev-server";
 import baseConfig from "./webpack.common";
 import paths from "../paths";
+
+// Need to declare the module webpack for getting type details correctly for webpack-merge v5
+// See: https://github.com/survivejs/webpack-merge/issues/141
+declare module "webpack" {
+	interface Configuration {
+		devServer?: WebpackDevServer.Configuration;
+	}
+}
 
 const config: webpack.Configuration = webpackMerge(baseConfig, {
 	mode: "development",
