1005453db23c53fd698d375dd4246db442efb196
Sourodeep Chatterjee <sourodeep224@outlook.com>
2020-08-08 21:28:26 +0530

    Added theme and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { CSSReset, ThemeProvider, ColorModeProvider } from "@chakra-ui/core";
+import Example from "./Example";
 
 export default function App(): JSX.Element {
-	return <div>Hello World</div>;
+	return (
+		<ThemeProvider>
+			<ColorModeProvider>
+				<CSSReset />
+				<Example />
+			</ColorModeProvider>
+		</ThemeProvider>
+	);
 }
